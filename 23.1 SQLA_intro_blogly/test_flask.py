5db001bf1fad008396f58d4bb0fef5c2cf288360
Joyce <hellojoycewong@gmail.com>
2021-01-14 12:37:58 -0800

    User Test cases

--- 23.1 SQLA_intro_blogly/test_flask.py
+++ 23.1 SQLA_intro_blogly/test_flask.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import db, Pet
+from models import db, User
 
 # Use test database and don't clutter tests with SQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
@@ -17,6 +17,54 @@ db.drop_all()
 db.create_all()
 
 class UsersViewsTestCase(TestCase):
-    """Test views for Users."""
 
-    
+    def setUp(self):
+        """Add sample user."""
+
+        User.query.delete()
+
+        user = User(first_name="TestFirstName", last_name="TESTLastName", image_url="www.google.com")
+        db.session.add(user)
+        db.session.commit()
+
+        self.user_id = user.id
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
+
+    """Test views for Users."""
+    def test_user_list(self):
+        with app.test_client() as client:
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('TestFirstName', html)
+
+    # def test_user_info(self):
+    #     with app.test_client() as client:
+    #         resp = client.get("/users/1")
+    #         html = resp.get_data(as_text=True)
+
+    #         self.assertEqual(resp.status_code, 200)
+    #         self.assertIn('<p>First Name: TestFirstName</p>', html)
+
+    # def test_new_user(self):
+    #     with app.test_client() as client:
+    #         d = {"first_name":"Test Person", "last_name":"LAST2TEST"}
+
+    #         resp = client.post("/users/new", data=d, follow_redirects=True)
+    #         html = resp.get_data(as_text=True)
+
+    #         self.assertEqual(resp.status_code, 302)
+    #         self.assertIn('Test Person', html)
+        
+    def test_add_pet(self):
+        with app.test_client() as client:
+            d = {"id":7, "first_name": "TestDelete", "last_name": "DeleteLast", "image_url": "www.hello.com"}
+            resp = client.post("/users/7/delete", data=d, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
