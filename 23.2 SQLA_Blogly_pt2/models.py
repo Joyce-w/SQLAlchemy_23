4883faecf7324cd2f6e9bbc028a6b2538468ec4b
Joyce <hellojoycewong@gmail.com>
2021-01-15 12:25:26 -0800

    Created post route & display post titles on user

--- 23.2 SQLA_Blogly_pt2/models.py
+++ 23.2 SQLA_Blogly_pt2/models.py
@@ -12,8 +12,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     def __repr__(self):
-        p = self
-        return f"<User id ={p.id} first_name = {p.first_name} last_name = {p.last_name}>"
+        u = self
+        return f"<User id ={u.id} first_name = {u.first_name} last_name = {u.last_name}>"
 
     #define indiv col in users table 
     id = db.Column(db.Integer,
@@ -29,3 +29,31 @@ class User(db.Model):
 
     #define col for image url
     image_url = db.Column(db.String)    
+
+class Post(db.Model):
+    #Create model for blog posts
+    __tablename__ = "post"
+
+    # def __repr__(self):
+    #     p = self
+    #     return f"<Post >"
+
+    #define pk col for post table
+    id = db.Column(db.Integer,
+                primary_key=True,
+                autoincrement=True)
+    
+    #define title col for post table
+    title = db.Column(db.String(50),
+                nullable=True)
+    
+    #define desc content for post table
+    content = db.Column(db.String(250))
+        
+    #create timestamp for post table 
+    created_at = db.Column(db.DateTime(),
+                nullable=True)
+    
+    #FK for User.id
+    user_id = db.Column(db.Integer,
+                db.ForeignKey('users.id'))
